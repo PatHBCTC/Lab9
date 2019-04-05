c70fc7aaaaa4bfbd29fa3c24af63e66e1bd1f9fc
Mike <46755443+msers@users.noreply.github.com>
2019-04-05 19:33:12 -0400

    Add files via upload

---
+++ Lab9SQLTest.java
@@ -0,0 +1,47 @@
+/*
+
+*
+
+* Lab 9 SQL Connection Test
+
+*
+
+*/
+
+import java.sql.*;
+
+public class Lab9SQLTest
+{
+
+   public static void main(String args[])
+   {
+   
+      final String DatabaseURL = "sql1.cis.mc3.edu";
+   
+      
+      try
+      {
+      // Create connection to SQL
+      Connection conn = DriverManager.getConnection(DatabaseURL);
+      System.out.println("Connected to: " + DatabaseURL);
+      
+      Statement stmt = conn.createStatement();
+      
+      String sqlStatement = "SELECT Student_FullName FROM Student";
+      System.out.println("Statement created: " + stmt);
+      
+      ResultSet result = stmt.ExecuteQuery(sqlStatement);
+      
+      
+      }
+      catch(Exception e)
+      {
+         
+         System.out.println(e);
+      
+      }
+   
+   
+   }
+
+}
